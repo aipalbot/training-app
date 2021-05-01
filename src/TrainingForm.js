084be8f89bd7ee2e3ab8d121dec0f1ea07a0dd0c
aipalbot <info@aipalbot.com>
2021-05-01 17:19:47 -0400

    capture form data

--- src/TrainingForm.js
+++ src/TrainingForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -12,19 +12,88 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+//create a model that looks like what API is expecting
+const trainingRequest = {
+  "costPerTraining": 5,
+"durationPerMonth":7
+}
+
 export default function TrainingForm() {
   const classes = useStyles();
 
-  return (
+  const [data, setData] = useState(trainingRequest);
+  const [cost, setCost] = useState("");
+ 
+
+  //create a function called handleInputChange
+  const handleInputChange = e => {
+
+    e.preventDefault();
+    const { name, value } = e.target;
+
+    setData({
+      ...data,
+      [name]: value
+    });
+
+  }
+
+
+
+
+    const submitClick = () => {
+      // compute hours per month
+      const numberOfWeeks = 4;
+      const numberOfHoursWeek = 4;
+      const hoursPerMonth = numberOfWeeks * numberOfHoursWeek;
+      const costPerHour = data.costPerTraining / (data.durationPerMonth * hoursPerMonth);
+      
+      setCost(costPerHour);
+      console.log(data);
+    }
+
+    return (
     <form className={classes.root} noValidate autoComplete="off">
       <div>
         <br></br>
-        <TextField label="Training Cost" id="standard-size-small" defaultValue="Cost Per Training" size="small" /> <br/>
-        <TextField label="Duration" id="standard-size-small" defaultValue="Duration (Months)" size="small" /><br/>
-        <TextField label="Cost/Hour" id="standard-size-small" defaultValue="Cost Per Hour" size="small" /> <br />
-        <Button variant="contained" color="primary">
+
+        <TextField 
+        name="costPerTraining"
+        value={data.costPerTraining} 
+        label="Training Cost" 
+        id="standard-size-small" 
+        placeholder="Cost Per Training" 
+        size="small"
+        onChange={handleInputChange}
+        /> <br/>
+
+        <TextField 
+        name="durationPerMonth"
+        value={data.durationPerMonth}
+        label="Duration" 
+        id="standard-size-small"
+        placeholder="Duration (Months)" 
+        size="small"       
+        onChange={handleInputChange}
+           /><br/>
+        
+
+        <TextField 
+        label="Cost/Hour" 
+        id="standard-size-small" 
+        placeholder="Cost Per Hour" 
+        size="small" 
+        name = "costPerHour"
+        value={cost}
+        /> <br />
+        
+        <Button 
+        variant="contained" 
+        color="secondary"
+        onClick={submitClick}>
         Calculate Cost
-      </Button>
+        </Button>
+
       </div>
       
     </form>
